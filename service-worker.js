926b114e44ca3c7419621b9ba549b7505ff67865
Aikke <1057346915@qq.com>
2026-09-24 00:50:56 +0800

    fix: refresh cached workout assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wenlian-v3';
+const CACHE_NAME = 'wenlian-v4';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './workout-data.js',
   './manifest.webmanifest', './offline.html', './icon-192.png', './icon-512.png',
